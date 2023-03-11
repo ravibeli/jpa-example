decaad311b254081ba8e799a8adcdbd1d390bf45
Sangamanath069 <sangamanathmuddebihal@gmail.com>
2023-03-11 11:25:13 +0530

    commit test

--- src/main/java/com/hr/app/jpaexample/controller/DepartmentController.java
+++ src/main/java/com/hr/app/jpaexample/controller/DepartmentController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/departments")
 public class DepartmentController {
 
+    
     @Autowired
     private DepartmentService departmentService;
 
